17b73c4a6032e4cc957ec6fe847dde27a17d88e0
Patrick Roche <rochep63@gmail.com>
2020-09-04 12:09:12 -0400

    add outline for packet structures

---
+++ software/include/protocol.hpp
@@ -0,0 +1,36 @@
+#ifndef PROTOCOL_HPP
+#define PROTOCL_HPP
+
+#include <cstdint>
+
+typedef struct _HEALTH_PKT
+{
+
+} HEALTH_PKT;
+
+typedef struct _IMU_PKT
+{
+
+} IMU_PKT;
+
+typedef struct _BARO_PKT
+{
+
+} BARO_PKT;
+
+typedef struct _GPS_PKT
+{
+
+} GPS_PKT;
+
+typedef struct _EVENT_PKT
+{
+
+} EVENT_PKT;
+
+typedef struct _CMD_PKT
+{
+
+} CMD_PKT;
+
+#endif /* PROTOCOL_HPP */
